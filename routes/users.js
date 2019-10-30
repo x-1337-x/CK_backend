77ccc9050bac4bddbafd0944867389acdc04656e
x-1337-x <x-1337-x@yandex.ru>
2019-10-30 02:01:40 +0100

    usr model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-router.post('/', (req, res) => {
-	res.send('Reg a user');
-});
+const User = require('../models/User');
+
+router.post(
+	'/',
+	[
+		check('name', 'name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'include a valid email').isEmail(),
+		check('password', 'pwd must be > 6 chars').isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.send('passed');
+	}
+);
 
 module.exports = router;
