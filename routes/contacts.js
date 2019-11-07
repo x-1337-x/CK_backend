c1845ec3cb9a5ddfd55b71d89a5976c408a5a3f7
x-1337-x <x-1337-x@yandex.ru>
2019-11-07 14:49:40 +0100

    add conntact

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-router.get('/', (req, res) => {
-	res.send('Get all user contacts');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
+router.get('/', auth, async (req, res) => {
+	try {
+		const contacts = await Contact.find({ user: req.user.id }).sort({
+			date: -1
+		});
+		res.json(contacts);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
 });
 
-router.post('/', (req, res) => {
-	res.send('Add new contact');
-});
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('name', 'Name is required')
+				.not()
+				.isEmpty()
+		]
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, phone, type } = req.body;
+
+		try {
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id
+			});
+
+			const contact = await newContact.save();
+
+			res.json(contact);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 router.put('/:id', (req, res) => {
 	res.send('Update contact');
