64cdfc1a42e2e9bbd060b653fc52a3b1e7e9af30
x-1337-x <x-1337-x@yandex.ru>
2020-01-31 20:41:59 +0100

    navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+	return (
+		<div className='navbar bg-primary'>
+			<i className={icon} /> {title}
+		</div>
+	);
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+	title: 'Contact Keeper',
+	icon: 'fas fa-id-card-alt'
+};
+
+export default Navbar;
