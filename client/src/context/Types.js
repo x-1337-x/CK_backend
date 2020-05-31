9957e2e65fe158d0af75b5bcd50ec923cf8e865b
x-1337-x <g1thub_acc@protonmail.com>
2020-05-31 16:42:12 +0200

    alerts

--- client/src/context/Types.js
+++ client/src/context/Types.js
@@ -6,7 +6,7 @@ export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 export const FILTER_CONTACTS = 'FILTER_CONTACTS';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 export const SET_ALERT = 'SET_ALERT';
-export const REAMOVE_ALERT = 'REAMOVE_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
 
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
