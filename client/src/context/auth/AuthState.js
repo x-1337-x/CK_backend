a668d9a30de719779f0346a9391e4607050767c3
x-1337-x <g1thub_acc@protonmail.com>
2020-06-01 10:59:50 +0200

    register user

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -24,14 +25,39 @@ const AuthState = (props) => {
 	const [state, dispatch] = useReducer(authReducer, initalState);
 
 	//Load User
+	const loadUser = () => console.log('loadUser');
 
 	// Register User
+	const register = async (formData) => {
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+			},
+		};
+
+		try {
+			const res = await axios.post('/api/users', formData, config);
+
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			});
+		} catch (error) {
+			dispatch({
+				type: REGISTER_FAIL,
+				payload: error.response.data.msg,
+			});
+		}
+	};
 
 	// Login User
+	const login = () => console.log('login');
 
 	// Logout
+	const logout = () => console.log('logout');
 
 	// Clear Errors
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
 	return (
 		<AuthContext.Provider
@@ -41,6 +67,11 @@ const AuthState = (props) => {
 				loading: state.loading,
 				user: state.user,
 				error: state.error,
+				register,
+				loadUser,
+				login,
+				logout,
+				clearErrors,
 			}}
 		>
 			{props.children}
