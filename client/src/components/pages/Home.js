bfe504a4edf9ba3a777226c362113918fc964711
x-1337-x <g1thub_acc@protonmail.com>
2020-06-02 13:44:22 +0200

    fixed home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,17 +2,35 @@ import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+	const authContext = useContext(AuthContext);
+	const { isAuthenticated } = authContext;
+
+	useEffect(() => {
+		console.log('loadUser');
+
+		authContext.loadUser();
+		// eslint-disable-next-line
+	}, []);
+
 	return (
-		<div className='grid-2'>
-			<div>
-				<ContactForm />
-			</div>
-			<div>
-				<ContactFilter />
-				<Contacts />
-			</div>
+		<div>
+			{isAuthenticated && (
+				<div className='grid-2'>
+					<div>
+						<ContactForm />
+					</div>
+					<div>
+						<ContactFilter />
+						<Contacts />
+					</div>
+				</div>
+			)}
+			{!isAuthenticated && (
+				<h1 style={{ 'text-align': 'center' }}>You are not logged in</h1>
+			)}
 		</div>
 	);
 };
