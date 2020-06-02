adf99a421af5f36dc8487a79b2bb2c2458551d49
x-1337-x <g1thub_acc@protonmail.com>
2020-06-02 13:23:24 +0200

    trying to fix home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,18 +2,8 @@ import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
-import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
-	const authContext = useContext(AuthContext);
-
-	useEffect(() => {
-		console.log('loadUser');
-
-		authContext.loadUser();
-		// eslint-disable-next-line
-	}, []);
-
 	return (
 		<div className='grid-2'>
 			<div>
