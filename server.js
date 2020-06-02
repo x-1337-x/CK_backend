fe1460dc70c53e6927358c2be0fc75253a2b6c00
x-1337-x <g1thub_acc@protonmail.com>
2020-06-02 13:04:24 +0200

    prepared for deployment to heroku, postbuild / fix: removed initial route

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ connectDB();
 //middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the CK API' }));
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
